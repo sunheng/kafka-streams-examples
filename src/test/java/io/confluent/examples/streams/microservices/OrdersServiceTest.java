3352d7878aa88207e95a1b62fa013b3e9762a38c
Matthias J. Sax <matthias@confluent.io>
2018-02-15 12:46:24 -0800

    WIP

--- src/test/java/io/confluent/examples/streams/microservices/OrdersServiceTest.java
+++ src/test/java/io/confluent/examples/streams/microservices/OrdersServiceTest.java
@@ -19,11 +19,11 @@ import javax.ws.rs.client.Entity;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.Response;
 import java.net.HttpURLConnection;
+import java.util.Collections;
 
 import static io.confluent.examples.streams.avro.microservices.Order.newBuilder;
 import static io.confluent.examples.streams.microservices.domain.beans.OrderId.id;
 import static io.confluent.examples.streams.microservices.util.MicroserviceUtils.MIN;
-import static java.util.Arrays.asList;
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON_TYPE;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.Assert.fail;
@@ -91,8 +91,8 @@ public class OrdersServiceTest extends MicroserviceTestUtils {
 
 
   @Test
-  public void shouldGetValidatedOrderOnRequest() throws Exception {
-    Order orderV1 = new Order(id(1L), 2L, OrderState.CREATED, Product.JUMPERS, 10, 100d);
+  public void shouldGetValidatedOrderOnRequest() {
+    Order orderV1 = new Order(id(1L), 3L, OrderState.CREATED, Product.JUMPERS, 10, 100d);
     OrderBean beanV1 = OrderBean.toBean(orderV1);
 
     final Client client = ClientBuilder.newClient();
@@ -108,7 +108,7 @@ public class OrdersServiceTest extends MicroserviceTestUtils {
         .post(Entity.json(beanV1));
 
     //Simulate the order being validated
-    MicroserviceTestUtils.sendOrders(asList(
+    MicroserviceTestUtils.sendOrders(Collections.singletonList(
         newBuilder(orderV1)
             .setState(OrderState.VALIDATED)
             .build()));
@@ -125,7 +125,7 @@ public class OrdersServiceTest extends MicroserviceTestUtils {
   }
 
   @Test
-  public void shouldTimeoutGetIfNoResponseIsFound() throws Exception {
+  public void shouldTimeoutGetIfNoResponseIsFound() {
     final Client client = ClientBuilder.newClient();
 
     //Start the rest interface
@@ -147,8 +147,8 @@ public class OrdersServiceTest extends MicroserviceTestUtils {
   }
 
   @Test
-  public void shouldGetOrderByIdWhenOnDifferentHost() throws Exception {
-    OrderBean order = new OrderBean(id(1L), 2L, OrderState.VALIDATED, Product.JUMPERS, 10, 100d);
+  public void shouldGetOrderByIdWhenOnDifferentHost() {
+    OrderBean order = new OrderBean(id(1L), 4L, OrderState.VALIDATED, Product.JUMPERS, 10, 100d);
     final Client client = ClientBuilder.newClient();
 
     //Given two rest servers on different ports
